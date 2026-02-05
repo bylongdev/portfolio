df147ad0b963f12b487ae246754372558b385451
NightW0lfff <philong170101@gmail.com>
2026-02-05 22:17:34 +1100

    refactor: Split Navbar into seperate component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import bg from "@/resources/imgs/city-landscape.jpg";
+import bg from "@/resources/imgs/city-landscape.png";
 import Image from "next/image";
 
 import NavBar from "@/components/navBar";
@@ -6,17 +6,25 @@ import NavBar from "@/components/navBar";
 export default function Home() {
   return (
     <div className="relative min-h-screen max-w-dvw">
-      <main className="relative flex h-screen max-h-300 w-full flex-col items-center">
+      <main className="flex h-screen min-h-dvh w-full flex-col items-center">
         {/* Navbar */}
         <NavBar />
 
         {/* Hero Section */}
-        <div className="bg-accent flex h-3/5 flex-col items-center justify-center">
-          <h1>Long Nguyen</h1>
-          <h2>Software Engineer</h2>
+        <div className="flex h-full w-full flex-col">
+          <div className="flex grow flex-col items-center justify-center gap-2">
+            <h1 className="text-5xl font-semibold capitalize">Long Nguyen</h1>
+            <h2 className="font-mono text-2xl tracking-wider">
+              Software Engineer
+            </h2>
+          </div>
+          {/* footer image */}
+          <div className=""></div>
         </div>
 
-        <Image src={bg} alt="Background Image" />
+        {/* About Section */}
+        {/* Features Section */}
+        {/* Projects Section */}
       </main>
     </div>
   );
--- components/navBar.tsx
+++ components/navBar.tsx
@@ -1,41 +1,41 @@
 import React from "react";
 import {
-	NavigationMenu,
-	NavigationMenuItem,
-	NavigationMenuLink,
-	NavigationMenuList,
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
 } from "@/components/ui/navigation-menu";
 import Link from "next/link";
 
 function NavBar() {
-	return (
-		<div className=" absolute top-0 flex w-full justify-center pt-6">
-			<NavigationMenu>
-				<NavigationMenuList className="uppercase ">
-					<NavigationMenuItem>
-						<NavigationMenuLink asChild>
-							<Link href={"/"}>About</Link>
-						</NavigationMenuLink>
-					</NavigationMenuItem>
-					<NavigationMenuItem>
-						<NavigationMenuLink asChild>
-							<Link href={"/"}>Projects</Link>
-						</NavigationMenuLink>
-					</NavigationMenuItem>
-					<NavigationMenuItem>
-						<NavigationMenuLink asChild>
-							<Link href={"/"}>Services</Link>
-						</NavigationMenuLink>
-					</NavigationMenuItem>
-					<NavigationMenuItem>
-						<NavigationMenuLink asChild>
-							<Link href={"/"}>Contact</Link>
-						</NavigationMenuLink>
-					</NavigationMenuItem>
-				</NavigationMenuList>
-			</NavigationMenu>
-		</div>
-	);
+  return (
+    <div className="absolute top-0 flex w-full justify-center pt-10">
+      <NavigationMenu>
+        <NavigationMenuList className="uppercase">
+          <NavigationMenuItem>
+            <NavigationMenuLink asChild>
+              <Link href={"/"}>About</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLink asChild>
+              <Link href={"/"}>Projects</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLink asChild>
+              <Link href={"/"}>Services</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLink asChild>
+              <Link href={"/"}>Contact</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
+  );
 }
 
 export default NavBar;
