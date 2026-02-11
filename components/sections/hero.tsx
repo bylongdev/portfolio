bd0f2ad8b32e798c3c966bca18e5ad35ab9476b6
NightW0lfff <philong170101@gmail.com>
2026-02-12 01:29:39 +1100

    feat: Link the CTA button in hero with sections.

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -1,25 +1,22 @@
 import React from "react";
 import { Button } from "../ui/button";
+import Link from "next/link";
 
 function HeroSection() {
   return (
-    // <div className="flex h-screen snap-center flex-col">
-    <>
-      <div className="flex grow flex-col items-center justify-center gap-2">
-        <h1 className="text-5xl font-semibold capitalize">Long Nguyen</h1>
-        <h2 className="font-mono text-2xl tracking-wider">Software Engineer</h2>
-        <p className="pt-4 pb-2">Building tools that solve real problems</p>
-        <div className="flex gap-2">
-          <Button className="hover:cursor-pointer">View projects</Button>
-          <Button variant={"outline"} className="hover:cursor-pointer">
-            Get in touch
-          </Button>
-        </div>
+    <div className="flex grow flex-col items-center justify-center gap-2">
+      <h1 className="text-5xl font-semibold capitalize">Long Nguyen</h1>
+      <h2 className="font-mono text-2xl tracking-wider">Software Engineer</h2>
+      <p className="pt-4 pb-2">Building tools that solve real problems</p>
+      <div className="flex gap-2">
+        <Button className="hover:cursor-pointer">
+          <Link href={"#project"}>View projects</Link>
+        </Button>
+        <Button variant={"outline"} className="hover:cursor-pointer">
+          <Link href={"#contact"}>Get in touch</Link>
+        </Button>
       </div>
-      {/* footer image */}
-      <div className=""></div>
-    </>
-    // </div>
+    </div>
   );
 }
 
