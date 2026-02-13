5c36a806a84afa7401e2585b9d930f88455b9333
NightW0lfff <philong170101@gmail.com>
2026-02-14 04:24:17 +1100

    update: Update validation for message content length limit.

--- components/forms/contact-form.tsx
+++ components/forms/contact-form.tsx
@@ -4,5 +4,8 @@ export const formSchema = z.object({
   fname: z.string(),
   lname: z.string(),
   email: z.email("Please enter a valid email address"),
-  message: z.string().optional(),
+  message: z
+    .string()
+    .min(10, "Message must be at least 10 characters")
+    .max(200, "Message cannot exceed 200 characters"),
 });
