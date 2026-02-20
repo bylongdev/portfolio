5c9689fc0ce6cb79db300371bbac98ec0bb34218
NightW0lfff <philong170101@gmail.com>
2026-02-21 01:42:50 +1100

    debug: Fix the z-index for the navbar to make sure it on top of every comps.

--- components/sections/navBar.tsx
+++ components/sections/navBar.tsx
@@ -9,7 +9,7 @@ import Link from "next/link";
 
 function NavBar() {
   return (
-    <div className="bg-muted/20 sticky top-0 z-10 mt-5 flex w-full justify-center py-5 backdrop-blur-md">
+    <div className="bg-muted/20 sticky top-0 z-20 mt-5 flex w-full justify-center py-5 backdrop-blur-md">
       <NavigationMenu>
         <NavigationMenuList className="uppercase">
           <NavigationMenuItem>
